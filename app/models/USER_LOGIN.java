3bc27590711429aa60cc840d62a5448ece381583
yoyo <sucahyo.t@gmail.com>
2015-05-18 18:02:21 -0700

    Commented USER_LOGIN models as mySQL database is not ready. This is tied up with the database declared in application.conf (but currently, it is being commented as well)

--- app/models/USER_LOGIN.java
+++ app/models/USER_LOGIN.java
@@ -1,15 +1,15 @@
 package models;
 
-import java.util.*;
-import javax.persistence.*;
+//import java.util.*;
+//import javax.persistence.*;
 
-import play.db.ebean.*;
-import play.data.validation.*;
+//import play.db.ebean.*;
+//import play.data.validation.*;
 
 /**
  * Created by admin on 18/04/15.
  */
-@entity
-@table(name="user")
-public class USER_LOGIN {
-}
+//@entity
+//@table(name="user")
+//public class USER_LOGIN {
+//}
